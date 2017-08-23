a9bc7cb4686b72751914e031f0a2d8f4f1eeb851
tsukanov-as <tsukanov.as@yandex.ru>
2017-08-23 20:15:34 +0300

    мелкие поправки в скриптах

--- build.ps1
+++ build.ps1
@@ -1,4 +1,4 @@
-Write-Progress -Activity "Build" -Status "$I% Complete:" -PercentComplete 0
+Write-Progress -Activity "Build" -Status "Progress:" -PercentComplete 0
 
 If(!(test-path .\build)) { (New-Item -ItemType Directory -Force -Path .\build) | Out-Null }
 Remove-Item .\build\* -recurse
@@ -6,19 +6,19 @@ Remove-Item .\build\* -recurse
 $ArgList = @('DESIGNER', '/F .\temp\', '/LoadExternalDataProcessorOrReportFromFiles .\src\BSL-Parser.xml .\build\BSL-Parser.epf')
 Start-Process 'C:\Program Files (x86)\1cv8\common\1cestart.exe' -ArgumentList $ArgList -Wait
 
-Write-Progress -Activity "Build" -Status "$I% Complete:" -PercentComplete 25
+Write-Progress -Activity "Build" -Status "Progress:" -PercentComplete 25
 
 $ArgList = @('DESIGNER', '/F .\temp\', '/LoadExternalDataProcessorOrReportFromFiles .\gui\src\gui.xml .\build\gui.epf')
 Start-Process 'C:\Program Files (x86)\1cv8\common\1cestart.exe' -ArgumentList $ArgList -Wait
 
-Write-Progress -Activity "Build" -Status "$I% Complete:" -PercentComplete 50
+Write-Progress -Activity "Build" -Status "Progress:" -PercentComplete 50
 
 $ArgList = @('DESIGNER', '/F .\temp\', '/LoadExternalDataProcessorOrReportFromFiles .\backends\BSL\src\BSL.xml .\build\BSL.epf')
 Start-Process 'C:\Program Files (x86)\1cv8\common\1cestart.exe' -ArgumentList $ArgList -Wait
 
-Write-Progress -Activity "Build" -Status "$I% Complete:" -PercentComplete 75
+Write-Progress -Activity "Build" -Status "Progress:" -PercentComplete 75
 
 $ArgList = @('DESIGNER', '/F .\temp\', '/LoadExternalDataProcessorOrReportFromFiles .\plugins\ReturnCheck\src\ReturnCheck.xml .\build\ReturnCheck.epf')
 Start-Process 'C:\Program Files (x86)\1cv8\common\1cestart.exe' -ArgumentList $ArgList -Wait
 
-Write-Progress -Activity "Build" -Status "$I% Complete:" -PercentComplete 100
+Write-Progress -Activity "Build" -Status "Progress:" -PercentComplete 100
--- explode.ps1
+++ explode.ps1
@@ -1,25 +1,25 @@
-Write-Progress -Activity "Extract" -Status "$I% Complete:" -PercentComplete 0
+Write-Progress -Activity "Extract" -Status "Progress:" -PercentComplete 0
 
-Remove-Item .\src -recurse
+Remove-Item .\src\* -recurse
 $ArgList = @('DESIGNER', '/F .\temp\', '/DumpExternalDataProcessorOrReportToFiles .\src\BSL-Parser.xml .\build\BSL-Parser.epf')
 Start-Process 'C:\Program Files (x86)\1cv8\common\1cestart.exe' -ArgumentList $ArgList -Wait
 
-Write-Progress -Activity "Extract" -Status "$I% Complete:" -PercentComplete 25
+Write-Progress -Activity "Extract" -Status "Progress:" -PercentComplete 25
 
-Remove-Item .\gui\src -recurse
+Remove-Item .\gui\src\* -recurse
 $ArgList = @('DESIGNER', '/F .\temp\', '/DumpExternalDataProcessorOrReportToFiles .\gui\src\gui.xml .\build\gui.epf')
 Start-Process 'C:\Program Files (x86)\1cv8\common\1cestart.exe' -ArgumentList $ArgList -Wait
 
-Write-Progress -Activity "Extract" -Status "$I% Complete:" -PercentComplete 50
+Write-Progress -Activity "Extract" -Status "Progress:" -PercentComplete 50
 
-Remove-Item .\backends\BSL\src -recurse
+Remove-Item .\backends\BSL\src\* -recurse
 $ArgList = @('DESIGNER', '/F .\temp\', '/DumpExternalDataProcessorOrReportToFiles .\backends\BSL\src\BSL.xml .\build\BSL.epf')
 Start-Process 'C:\Program Files (x86)\1cv8\common\1cestart.exe' -ArgumentList $ArgList -Wait
 
-Write-Progress -Activity "Extract" -Status "$I% Complete:" -PercentComplete 75
+Write-Progress -Activity "Extract" -Status "Progress:" -PercentComplete 75
 
-Remove-Item .\plugins\ReturnCheck\src -recurse
+Remove-Item .\plugins\ReturnCheck\src\* -recurse
 $ArgList = @('DESIGNER', '/F .\temp\', '/DumpExternalDataProcessorOrReportToFiles .\plugins\ReturnCheck\src\ReturnCheck.xml .\build\ReturnCheck.epf')
 Start-Process 'C:\Program Files (x86)\1cv8\common\1cestart.exe' -ArgumentList $ArgList -Wait
 
-Write-Progress -Activity "Extract" -Status "$I% Complete:" -PercentComplete 100
+Write-Progress -Activity "Extract" -Status "Progress:" -PercentComplete 100
