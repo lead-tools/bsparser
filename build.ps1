06d515965af6863d029493cf9119f3875462403f
tsukanov-as <tsukanov.as@yandex.ru>
2017-12-10 01:52:51 +0300

    переделал скрипты с использованием режима агента #38

--- build.ps1
+++ build.ps1
@@ -1,24 +1,60 @@
-Write-Progress -Activity "Build" -Status "Progress:" -PercentComplete 0
 
-If(!(test-path .\build)) { (New-Item -ItemType Directory -Force -Path .\build) | Out-Null }
-Remove-Item .\build\* -recurse
+function wait($ssh) {
+    while (-not $ssh.DataAvailable) {
+        Start-Sleep -m 100
+    }
+}
 
-$ArgList = @('DESIGNER', '/F .\temp\', '/LoadExternalDataProcessorOrReportFromFiles .\src\BSLParser.xml .\build\BSLParser.epf')
-Start-Process 'C:\Program Files (x86)\1cv8\common\1cestart.exe' -ArgumentList $ArgList -Wait
+function send($ssh, $cmd) {
+    "#cmd: $cmd" >> log.txt
+    $ssh.WriteLine($cmd)
+    wait($ssh)
+    $res = ConvertFrom-Json $ssh.Read()
+    $res | Format-Table | Out-File log.txt -Append
+    if (-not ($res | where {$_.type -eq 'success'})) {
+        [System.Windows.Forms.MessageBox]::Show('An error has occured. Please see log file') | Out-Null
+        $res | where {$_.type -eq 'error'} | Write-Host
+        throw
+    }
+}
 
-Write-Progress -Activity "Build" -Status "Progress:" -PercentComplete 25
+function complete($percent) {
+    Write-Progress -Activity "Build" -Status "Progress:" -PercentComplete $percent
+}
 
-$ArgList = @('DESIGNER', '/F .\temp\', '/LoadExternalDataProcessorOrReportFromFiles .\gui\src\gui.xml .\build\gui.epf')
-Start-Process 'C:\Program Files (x86)\1cv8\common\1cestart.exe' -ArgumentList $ArgList -Wait
+complete 0
 
-Write-Progress -Activity "Build" -Status "Progress:" -PercentComplete 50
+"#build.ps1 START $(Get-Date)`n`n" >> log.txt
 
-$ArgList = @('DESIGNER', '/F .\temp\', '/LoadExternalDataProcessorOrReportFromFiles .\backends\BSL\src\BSL.xml .\build\BSL.epf')
-Start-Process 'C:\Program Files (x86)\1cv8\common\1cestart.exe' -ArgumentList $ArgList -Wait
+$UserName = 'admin'
+$EmptyPassword = New-object System.Security.SecureString
+$PSCredential = New-Object System.Management.Automation.PSCredential($UserName, $EmptyPassword)
 
-Write-Progress -Activity "Build" -Status "Progress:" -PercentComplete 75
+$ArgList = @('DESIGNER', '/F .\temp\', '/AgentMode', '/AgentSSHHostKeyAuto', '/AgentBaseDir .\')
+#$ArgList += '/Visible'
+Start-Process 'C:\Program Files (x86)\1cv8\common\1cestart.exe' -ArgumentList $ArgList
 
-$ArgList = @('DESIGNER', '/F .\temp\', '/LoadExternalDataProcessorOrReportFromFiles .\plugins\ReturnCheck\src\ReturnCheck.xml .\build\ReturnCheck.epf')
-Start-Process 'C:\Program Files (x86)\1cv8\common\1cestart.exe' -ArgumentList $ArgList -Wait
+Start-Sleep 1
 
-Write-Progress -Activity "Build" -Status "Progress:" -PercentComplete 100
+$1c = New-SSHSession 127.0.0.1 -Port 1543 -Credential $PSCredential
+$ssh = $1c | New-SSHShellStream
+wait($ssh)
+$ssh.Read() | Out-Null
+
+send $ssh 'options set --output-format=json --show-prompt=no'
+send $ssh 'common connect-ib'
+complete 20
+send $ssh 'config load-ext-files --file="..\src\BSLParser.xml" --ext-file="..\build\BSLParser.epf"'
+complete 40
+send $ssh 'config load-ext-files --file="..\gui\src\gui.xml" --ext-file="..\build\gui.epf"'
+complete 60
+send $ssh 'config load-ext-files --file="..\backends\BSL\src\BSL.xml" --ext-file="..\build\BSL.epf"'
+complete 80
+send $ssh 'config load-ext-files --file="..\plugins\ReturnCheck\src\ReturnCheck.xml" --ext-file="..\build\ReturnCheck.epf"'
+complete 100
+send $ssh 'common disconnect-ib'
+$ssh.WriteLine('common shutdown')
+wait($ssh)
+
+$1c | Remove-SSHSession | Out-Null
+"#build.ps1 END $(Get-Date)`n`n" >> log.txt
--- explode.ps1
+++ explode.ps1
@@ -1,25 +1,69 @@
-Write-Progress -Activity "Extract" -Status "Progress:" -PercentComplete 0
+
+function wait($ssh) {
+    while (-not $ssh.DataAvailable) {
+        Start-Sleep -m 100
+    }
+}
+
+function send($ssh, $cmd) {
+    "#cmd: $cmd" >> log.txt
+    $ssh.WriteLine($cmd)
+    wait($ssh)
+    $res = ConvertFrom-Json $ssh.Read()
+    $res | Format-Table | Out-File log.txt -Append
+    if (-not ($res | where {$_.type -eq 'success'})) {
+        [System.Windows.Forms.MessageBox]::Show('An error has occured. Please see log file') | Out-Null
+        $res | where {$_.type -eq 'error'} | Write-Host
+        throw
+    }
+}
+
+function complete($percent) {
+    Write-Progress -Activity "Extract" -Status "Progress:" -PercentComplete $percent
+}
+
+complete 0
+
+"#explode.ps1 START $(Get-Date)`n`n" >> log.txt
+
+$UserName = 'admin'
+$EmptyPassword = New-object System.Security.SecureString
+$PSCredential = New-Object System.Management.Automation.PSCredential($UserName, $EmptyPassword)
+
+$ArgList = @('DESIGNER', '/F .\temp\', '/AgentMode', '/AgentSSHHostKeyAuto', '/AgentBaseDir .\')
+#$ArgList += '/Visible'
+Start-Process 'C:\Program Files (x86)\1cv8\common\1cestart.exe' -ArgumentList $ArgList
+
+Start-Sleep 1
+
+$1c = New-SSHSession 127.0.0.1 -Port 1543 -Credential $PSCredential
+$ssh = $1c | New-SSHShellStream
+wait($ssh)
+$ssh.Read() | Out-Null
+
+send $ssh 'options set --output-format=json --show-prompt=no'
+send $ssh 'common connect-ib'
+complete 20
 
 Remove-Item .\src\* -recurse
-$ArgList = @('DESIGNER', '/F .\temp\', '/DumpExternalDataProcessorOrReportToFiles .\src\BSLParser.xml .\build\BSLParser.epf')
-Start-Process 'C:\Program Files (x86)\1cv8\common\1cestart.exe' -ArgumentList $ArgList -Wait
-
-Write-Progress -Activity "Extract" -Status "Progress:" -PercentComplete 25
+send $ssh 'config dump-ext-files --file="..\src\BSLParser.xml" --ext-file="..\build\BSLParser.epf"'
+complete 40
 
 Remove-Item .\gui\src\* -recurse
-$ArgList = @('DESIGNER', '/F .\temp\', '/DumpExternalDataProcessorOrReportToFiles .\gui\src\gui.xml .\build\gui.epf')
-Start-Process 'C:\Program Files (x86)\1cv8\common\1cestart.exe' -ArgumentList $ArgList -Wait
-
-Write-Progress -Activity "Extract" -Status "Progress:" -PercentComplete 50
+send $ssh 'config dump-ext-files --file="..\gui\src\gui.xml" --ext-file="..\build\gui.epf"'
+complete 60
 
 Remove-Item .\backends\BSL\src\* -recurse
-$ArgList = @('DESIGNER', '/F .\temp\', '/DumpExternalDataProcessorOrReportToFiles .\backends\BSL\src\BSL.xml .\build\BSL.epf')
-Start-Process 'C:\Program Files (x86)\1cv8\common\1cestart.exe' -ArgumentList $ArgList -Wait
-
-Write-Progress -Activity "Extract" -Status "Progress:" -PercentComplete 75
+send $ssh 'config dump-ext-files --file="..\backends\BSL\src\BSL.xml" --ext-file="..\build\BSL.epf"'
+complete 80
 
 Remove-Item .\plugins\ReturnCheck\src\* -recurse
-$ArgList = @('DESIGNER', '/F .\temp\', '/DumpExternalDataProcessorOrReportToFiles .\plugins\ReturnCheck\src\ReturnCheck.xml .\build\ReturnCheck.epf')
-Start-Process 'C:\Program Files (x86)\1cv8\common\1cestart.exe' -ArgumentList $ArgList -Wait
+send $ssh 'config dump-ext-files --file="..\plugins\ReturnCheck\src\ReturnCheck.xml" --ext-file="..\build\ReturnCheck.epf"'
+complete 100
 
-Write-Progress -Activity "Extract" -Status "Progress:" -PercentComplete 100
+send $ssh 'common disconnect-ib'
+$ssh.WriteLine('common shutdown')
+wait($ssh)
+
+$1c | Remove-SSHSession | Out-Null
+"#explode.ps1 END $(Get-Date)`n`n" >> log.txt
