bf57c7553c922801b08fb043b504c398e9ebd87d
tsukanov.as <tsukanov.as@yandex.ru>
2020-04-10 10:50:24 +0300

    Переделал скрипты сборки/разборки (#122)

--- build.ps1
+++ build.ps1
@@ -1,19 +1,4 @@
 ﻿
 . .\common.ps1
 
-complete 0 "Сборка"
-
-"#build.ps1 START $(Get-Date)`n`n" > log.txt
-
-$1c, $ssh = connect
-
-$x = 0; $dx = 100 / $list.Count
-foreach ($item in $list.GetEnumerator()) {
-    send $ssh "config load-ext-files --file=""..$($item.Value)src\$($item.Name).xml"" --ext-file=""..\build\$($item.Name).epf"""
-    $x += $dx
-    complete $x "Сборка"
-}
-
-disconnect $1c $ssh
-
-"#build.ps1 END $(Get-Date)`n`n" >> log.txt
+run LoadExternalDataProcessorOrReportFromFiles Сборка
--- common.ps1
+++ common.ps1
@@ -1,78 +1,47 @@
-﻿# [System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms") | Out-Null
-
-Remove-Variable * -ErrorAction SilentlyContinue; Remove-Module *; $error.Clear() #; Clear-Host
-
-function wait($ssh) {
-    # похоже ожидание не всегда работает на 15 платформе
-    # как минимум точно не работает при распаковке (explode.ps1)
-    # гипотетически надо не просто ждать данные, а еще и success в этих данных
-    while (-not $ssh.DataAvailable) {
-        Start-Sleep -m 100
-    }
+﻿
+$1CPath = "C:\Program Files\1cv8\8.3.15.1778\bin\1cv8.exe"
+if (-not (Test-Path $1CPath)) {
+    $1CPath = 'C:\Program Files\1cv8\common\1cestart.exe'
 }
-
-function send($ssh, $cmd) {
-    "#cmd: $cmd" >> log.txt
-    $ssh.WriteLine($cmd)
-    wait($ssh)
-    $data = $ssh.Read()
-    $data2 = $data -replace "\]\[", "," # фикс ошибки в json от платформы
-    $res = ConvertFrom-Json $data2 -WarningAction SilentlyContinue
-    $res | Format-Table | Out-File log.txt -Append
-    $err = $res | Where-Object {$_.type -eq 'error'}
-    if ($err) {
-        $data | Write-Host
-        # [System.Windows.Forms.MessageBox]::Show('An error has occured. Please see log file') | Out-Null
-    }
+if (-not (Test-Path $1CPath)) {
+    $1CPath = 'C:\Program Files (x86)\1cv8\common\1cestart.exe'
 }
 
 function complete($percent, $activity) {
     Write-Progress -Activity $activity -Status "Прогресс:" -PercentComplete $percent
 }
 
-function connect() {
-    $UserName = 'admin'
-    $EmptyPassword = New-object System.Security.SecureString
-    $PSCredential = New-Object System.Management.Automation.PSCredential($UserName, $EmptyPassword)
-
-    $ArgList = @('DESIGNER', '/F .\temp\', '/AgentMode', '/AgentSSHHostKeyAuto', '/AgentBaseDir .\')
-    # $ArgList += '/Visible'
-    $1cpath = 'C:\Program Files (x86)\1cv8\common\1cestart.exe'
-    if (-not (Test-Path $1cpath)) {
-        $1cpath = 'C:\Program Files\1cv8\common\1cestart.exe'
-    }
-    Start-Process $1cpath -ArgumentList $ArgList
-
-    Start-Sleep 1
-
-    $1c = New-SSHSession 127.0.0.1 -Port 1543 -Credential $PSCredential -AcceptKey
-    $ssh = $1c | New-SSHShellStream
-    wait($ssh)
-    $ssh.Read() | Out-Null
-
-    send $ssh 'options set --output-format=json --show-prompt=no'
-    send $ssh 'common connect-ib'
-
-    return $1c, $ssh
-}
-
-function disconnect($1c, $ssh) {
-    send $ssh 'common disconnect-ib' #TODO: эта команда похоже ничего не возвращает (надо убрать wait в итоге)
-    Start-Sleep 1
-    send $ssh 'common shutdown'
-    $1c | Remove-SSHSession | Out-Null
-}
-
 $list = @{
-    ПарсерВстроенногоЯзыка = "\"
-    gui                    = "\gui\"
-    ГенераторДокументацииДляПарсера = "\plugins\ГенераторДокументацииДляПарсера\"
-    ГенераторИсходногоКодаПоАСД = "\plugins\ГенераторИсходногоКодаПоАСД\"
-    ДетекторФункцийБезВозвратаВКонце = "\plugins\ДетекторФункцийБезВозвратаВКонце\"
-    ДетекторНеиспользуемыхПеременных = "\plugins\ДетекторНеиспользуемыхПеременных\"
-    ДетекторОшибочныхЗамыкающихКомментариев = "\plugins\ДетекторОшибочныхЗамыкающихКомментариев\"
-    РекурсивныйПодсчетСерверныхВызововВМодуляхФорм = "\plugins\РекурсивныйПодсчетСерверныхВызововВМодуляхФорм\"
-    ПодсчетКогнитивнойСложностиМетодов = "\plugins\ПодсчетКогнитивнойСложностиМетодов\"
-    ПримерМодификацииИсходногоКода = "\plugins\ПримерМодификацииИсходногоКода\"
-    TestRunner             = "\tests\TestRunner\"
+    ПарсерВстроенногоЯзыка                         = "\src"
+    Консоль                                        = "\gui\src"
+    Тестер                                         = "\tests\Тестер\src"
+    ГенераторДокументацииДляПарсера                = "\plugins\ГенераторДокументацииДляПарсера\src"
+    ГенераторИсходногоКодаПоАСД                    = "\plugins\ГенераторИсходногоКодаПоАСД\src"
+    ДетекторФункцийБезВозвратаВКонце               = "\plugins\ДетекторФункцийБезВозвратаВКонце\src"
+    ДетекторНеиспользуемыхПеременных               = "\plugins\ДетекторНеиспользуемыхПеременных\src"
+    ДетекторОшибочныхЗамыкающихКомментариев        = "\plugins\ДетекторОшибочныхЗамыкающихКомментариев\src"
+    РекурсивныйПодсчетСерверныхВызововВМодуляхФорм = "\plugins\РекурсивныйПодсчетСерверныхВызововВМодуляхФорм\src"
+    ПодсчетКогнитивнойСложностиМетодов             = "\plugins\ПодсчетКогнитивнойСложностиМетодов\src"
+    ПримерМодификацииИсходногоКода                 = "\plugins\ПримерМодификацииИсходногоКода\src"
+}
+
+function run ($command, $description){
+
+    complete 0 $description
+
+    $x = 0; $dx = 100 / $list.Count
+    foreach ($item in $list.GetEnumerator()) {
+        $ArgList =  "DESIGNER", "/DumpResult designer_result.txt", "/Out designer_out.txt",
+                    "/DisableStartupDialogs",
+                    "/F .\temp\",
+                    "/N Admin",
+                    "/$command `".$($item.Value)\$($item.Name).xml`" `".\build\$($item.Name).epf`""
+        Start-Process $1CPath -ArgumentList $ArgList -Wait
+        if ((Get-Content .\designer_result.txt) -ne '0') {
+            Get-Content .\designer_out.txt
+        }
+        $x += $dx
+        complete $x $description
+    }
+
 }
